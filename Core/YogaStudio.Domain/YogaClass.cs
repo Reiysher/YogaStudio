0d76cb06c99ef4a6a8de8c971efd84eb58d6e684
Maksim Kushniruk <reiysher25@gmail.com>
2021-10-29 01:39:31 +0300

    Add project files.

---
+++ Core/YogaStudio.Domain/YogaClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YogaStudio.Domain
+{
+    public class YogaClass
+    {
+        // class id
+        public Guid Id { get; set; }
+        // mentor(user) id
+        public Guid MentorId { get; set; }
+        // list of clients(users) id
+        public IEnumerable<Guid> UsersIds { get; set; }
+        // class date
+        
+        // class status
+
+        // class type
+
+        // class description
+    }
+}
