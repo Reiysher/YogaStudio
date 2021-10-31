52dfa7668d70b70178682e95c1f7bb5cccf14092
Maksim Kushniruk <reiysher25@gmail.com>
2021-10-31 20:11:52 +0300

    Added update yoga class command and handler

---
+++ Core/YogaStudio.Application/Features/YogaClasses/Commands/UpdateYogaClass/UpdateYogaClassCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using YogaStudio.Domain;
+using YogaStudio.Domain.Enums;
+using MediatR;
+
+namespace YogaStudio.Application.Features.YogaClasses.Commands.UpdateYogaClass
+{
+    public class UpdateYogaClassCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public Guid MentorId { get; set; }
+        public IEnumerable<Subscription> Subscriptions { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Description { get; set; }
+        public int MinClients { get; set; }
+        public int MaxClients { get; set; }
+        public YogaClassType Type { get; set; }
+        public YogaClassStatus Status { get; set; }
+    }
+}
---
+++ Core/YogaStudio.Application/Features/YogaClasses/Commands/UpdateYogaClass/UpdateYogaClassCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using YogaStudio.Application.Common.Exceptions;
+using YogaStudio.Application.Interfaces;
+using YogaStudio.Domain;
+
+namespace YogaStudio.Application.Features.YogaClasses.Commands.UpdateYogaClass
+{
+    public class UpdateYogaClassCommandHandler
+        : IRequestHandler<UpdateYogaClassCommand>
+    {
+        private readonly IYogaStudioDbContext _context;
+
+        public UpdateYogaClassCommandHandler(IYogaStudioDbContext context)
+        {
+            _context = context;
+        }
+
+        public async  Task<Unit> Handle(UpdateYogaClassCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var entity = await _context.Classes
+                .FirstOrDefaultAsync(c => c.Id == request.Id, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(YogaClass), request.Id);
+            }
+
+            entity.MentorId = request.MentorId;
+            entity.Subscriptions = request.Subscriptions;
+            entity.StartDate = request.StartDate;
+            entity.Description = request.Description;
+            entity.MinClients = request.MinClients;
+            entity.MaxClients = request.MaxClients;
+            entity.Type = request.Type;
+            entity.Status = request.Status;
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
