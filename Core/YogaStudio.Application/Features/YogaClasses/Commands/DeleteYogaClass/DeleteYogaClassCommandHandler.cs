aa706e05955cf7b6f0e8f99aef2bc3805d380e67
Maksim Kushniruk <reiysher25@gmail.com>
2021-10-31 20:31:00 +0300

    Added delete yoga class command and handler

---
+++ Core/YogaStudio.Application/Features/YogaClasses/Commands/DeleteYogaClass/DeleteYogaClassCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using YogaStudio.Application.Common.Exceptions;
+using YogaStudio.Application.Interfaces;
+using YogaStudio.Domain;
+
+namespace YogaStudio.Application.Features.YogaClasses.Commands.DeleteYogaClass
+{
+    public class DeleteYogaClassCommandHandler
+        : IRequestHandler<DeleteYogaClassCommand>
+    {
+        private readonly IYogaStudioDbContext _context;
+
+        public DeleteYogaClassCommandHandler(IYogaStudioDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(DeleteYogaClassCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var entity = await _context.Classes
+                .FirstOrDefaultAsync(c => c.Id == request.Id, cancellationToken);
+
+            if (entity == null)
+                throw new NotFoundException(nameof(YogaClass), request.Id);
+
+            _context.Classes.Remove(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
