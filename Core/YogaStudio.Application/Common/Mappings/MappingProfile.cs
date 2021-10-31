649deceebdd41993149ac03e222be708a1f1f22b
Maksim Kushniruk <reiysher25@gmail.com>
2021-10-31 18:32:30 +0300

    Added mapper profiles

---
+++ Core/YogaStudio.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YogaStudio.Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile(Assembly assembly)
+        {
+            ApplyMappings(assembly);
+        }
+
+        private void ApplyMappings(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(t => t.GetInterfaces()
+                    .Any(i => i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == typeof(IMapWith<>)));
+            foreach(var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodinfo = type.GetMethod("Mapping");
+                methodinfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
