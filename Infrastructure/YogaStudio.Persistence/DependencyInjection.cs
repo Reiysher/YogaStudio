e5451bfbb53527e376bbcda331c4009222c7e2af
Maksim Kushniruk <reiysher25@gmail.com>
2021-11-01 01:50:23 +0300

    Added method for registration db context in services

---
+++ Infrastructure/YogaStudio.Persistence/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YogaStudio.Application.Interfaces;
+
+namespace YogaStudio.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(
+            IServiceCollection services, IConfiguration configuration)
+        {
+            var connection = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<YogaStudioDbContext>(options =>
+            {
+                options.UseSqlServer(connection);
+            });
+            services.AddScoped<IYogaStudioDbContext>(provider =>
+                provider.GetRequiredService<YogaStudioDbContext>());
+            //TODO: DbContext dependency like this?
+            //services.AddScoped<IYogaStudioDbContext, YogaStudioDbContext>();
+            return services;
+        }
+    }
+}
