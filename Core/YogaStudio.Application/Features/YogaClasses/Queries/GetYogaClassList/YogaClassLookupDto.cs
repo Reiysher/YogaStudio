dd7840cdf0d50a55e3d8bb8008700ba36cbb9def
Maksim Kushniruk <reiysher25@gmail.com>
2021-11-01 01:09:53 +0300

    Added yoga class list query, handler, view model and lookup

---
+++ Core/YogaStudio.Application/Features/YogaClasses/Queries/GetYogaClassList/YogaClassLookupDto.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using YogaStudio.Application.Common.Mappings;
+using YogaStudio.Domain;
+
+namespace YogaStudio.Application.Features.YogaClasses.Queries.GetYogaClassList
+{
+    public class YogaClassLookupDto : IMapWith<YogaClass>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        // TODO: Add more fields
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<YogaClass, YogaClassLookupDto>()
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.Title, opt => opt.MapFrom(src => src.Title));
+        }
+    }
+}
