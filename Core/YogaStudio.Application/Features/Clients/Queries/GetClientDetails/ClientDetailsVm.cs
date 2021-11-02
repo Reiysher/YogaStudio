596f04b1b75835168b353bdc386807ca1328a57a
Maksim Kushniruk <reiysher25@gmail.com>
2021-11-02 20:05:04 +0300

    Added client details view model

---
+++ Core/YogaStudio.Application/Features/Clients/Queries/GetClientDetails/ClientDetailsVm.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YogaStudio.Application.Common.Mappings;
+using YogaStudio.Domain;
+
+namespace YogaStudio.Application.Features.Clients.Queries.GetClientDetails
+{
+    public class ClientDetailsVm : IMapWith<Client>
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public IEnumerable<Subscription> Subscriptions { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Client, ClientDetailsVm>()
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dst => dst.LastName, opt => opt.MapFrom(src => src.LastName))
+                .ForMember(dst => dst.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+                .ForMember(dst => dst.Subscriptions, opt => opt.MapFrom(src => src.Subscriptions));
+        }
+    }
+}
