c39e82a97f700ba1c088650f1acc024dc2b381d1
Maksim Kushniruk <reiysher25@gmail.com>
2021-11-03 00:28:36 +0300

    Added YogaStudioExceptionHandlerMiddleware for exception handling

---
+++ Backend/Presentation/YogaStudio.WebApi/Middleware/YogaStudioExceptionHandlerMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using YogaStudio.Application.Common.Exceptions;
+
+namespace YogaStudio.WebApi.Middleware
+{
+    public class YogaStudioExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public YogaStudioExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsynk(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = string.Empty;
+            switch (exception)
+            {
+                case ValidationException validationException:
+                    code = HttpStatusCode.BadRequest;
+                    result = JsonSerializer.Serialize(validationException.Errors);
+                    break;
+                case NotFoundException:
+                    code = HttpStatusCode.NotFound;
+                    break;
+            }
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            if(result == string.Empty)
+                result = JsonSerializer.Serialize(new { error = exception.Message });
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
