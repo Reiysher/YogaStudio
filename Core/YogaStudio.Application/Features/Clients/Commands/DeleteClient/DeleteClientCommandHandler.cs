cc6d46cc42a7a8d9d07a4f5159aaec3172e417cf
Maksim Kushniruk <reiysher25@gmail.com>
2021-11-02 19:50:56 +0300

    Added delete client command handler

---
+++ Core/YogaStudio.Application/Features/Clients/Commands/DeleteClient/DeleteClientCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YogaStudio.Application.Common.Exceptions;
+using YogaStudio.Application.Interfaces;
+using YogaStudio.Domain;
+
+namespace YogaStudio.Application.Features.Clients.Commands.DeleteClient
+{
+    public class DeleteClientCommandHandler
+        : IRequestHandler<DeleteClientCommand>
+    {
+        private readonly IYogaStudioDbContext _context;
+
+        public DeleteClientCommandHandler(IYogaStudioDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteClientCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var client = await _context.Clients
+                .FirstOrDefaultAsync(c => c.Id == request.Id, cancellationToken);
+
+            if(client == null)
+                throw new NotFoundException(nameof(Client), request.Id);
+
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
