20ce38f200e8ea28dbc668ad3a7fda0b5bba4ca5
Maksim Kushniruk <reiysher25@gmail.com>
2021-11-02 20:37:04 +0300

    Added get clients list query handler

--- Core/YogaStudio.Application/Features/Clients/Queries/GetClientsList/ClientsListVm.cs
+++ Core/YogaStudio.Application/Features/Clients/Queries/GetClientsList/ClientsListVm.cs
@@ -1,5 +1,4 @@
-﻿using MediatR;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace YogaStudio.Application.Features.Clients.Queries.GetClientsList
 {
-    public class GetClientListQuery : IRequest<ClientListVm>
+    public class ClientsListVm
     {
+        public IList<ClientLookupDto> Clients { get; set; }
     }
 }
---
+++ Core/YogaStudio.Application/Features/Clients/Queries/GetClientsList/GetClientsListQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YogaStudio.Application.Interfaces;
+
+namespace YogaStudio.Application.Features.Clients.Queries.GetClientsList
+{
+    public class GetClientsListQueryHandler
+        : IRequestHandler<GetClientsListQuery, ClientsListVm>
+    {
+        private readonly IYogaStudioDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetClientsListQueryHandler(IYogaStudioDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<ClientsListVm> Handle(GetClientsListQuery request, 
+            CancellationToken cancellationToken)
+        {
+            var clients = await _context.Clients
+                .ProjectTo<ClientLookupDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return new ClientsListVm { Clients = clients };
+        }
+    }
+}
