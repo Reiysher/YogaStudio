6630cafafe02ec5a2e9d722167243a4b685593e1
Maksim Kushniruk <reiysher25@gmail.com>
2021-11-02 19:54:27 +0300

    Added update client command

---
+++ Core/YogaStudio.Application/Features/Clients/Commands/UpdateClient/UpdateClientCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using YogaStudio.Domain;
+
+namespace YogaStudio.Application.Features.Clients.Commands.UpdateClient
+{
+    public class UpdateClientCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public IEnumerable<Subscription> Subscriptions { get; set; }
+    }
+}
