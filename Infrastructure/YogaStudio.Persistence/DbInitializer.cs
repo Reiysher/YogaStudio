0a4c8cc91d57d7047a5b6a52730747a06269ad4b
Maksim Kushniruk <reiysher25@gmail.com>
2021-11-02 19:37:59 +0300

    Added create client command

--- Infrastructure/YogaStudio.Persistence/DbInitializer.cs
+++ Infrastructure/YogaStudio.Persistence/DbInitializer.cs
@@ -4,6 +4,7 @@
     {
         public static void Initialize(YogaStudioDbContext context)
         {
+            // TODO: Use this method for initialize database with data
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
         }
