212e46edf548440157e7ad2b8284e2f8848ab8cb
Maksim Kushniruk <reiysher25@gmail.com>
2021-11-03 19:33:24 +0300

    Added login view model, view, and login method into the auth controller

---
+++ Identity/YogaStudio.Identity/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YogaStudio.Identity.Models;
+
+namespace YogaStudio.Identity.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IIdentityServerInteractionService _interactionService;
+
+        public AuthController(SignInManager<AppUser> signInManager, 
+            UserManager<AppUser> userManager,
+            IIdentityServerInteractionService interactionService) 
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _interactionService = interactionService;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            var model = new LoginViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
