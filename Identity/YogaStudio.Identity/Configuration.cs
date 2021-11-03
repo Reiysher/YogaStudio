86ba9467b6c6d340899559694f2a57334e80b50c
Maksim Kushniruk <reiysher25@gmail.com>
2021-11-03 14:26:34 +0300

    Added IS4 configuration

---
+++ Identity/YogaStudio.Identity/Configuration.cs
@@ -0,0 +1,67 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YogaStudio.Identity
+{
+    public class Configuration
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("YogaStudioWebAPI", "Web API")
+            };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource("YogaStudioWebAPI", "Web API", new [] { JwtClaimTypes.Name })
+                {
+                    Scopes = { "YogaStudioWebAPI" }
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "yoga-studio-web-api",
+                    ClientName = "Yoga Studio Web",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    RequirePkce = true,
+                    RedirectUris =
+                    {
+                        "http://.../signin-oidc"
+                    },
+                    AllowedCorsOrigins =
+                    {
+                        "http://..."
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "http://.../signout-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "YogaStudioWebAPI"
+                    },
+                    AllowAccessTokensViaBrowser = true
+                }
+            };
+    }
+}
