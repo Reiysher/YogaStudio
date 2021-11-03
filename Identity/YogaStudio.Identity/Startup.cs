2849b72634ffd1604e34e8dd5d24d2aa96347314
Maksim Kushniruk <reiysher25@gmail.com>
2021-11-03 13:23:14 +0300

    Added IdentityServer4

---
+++ Identity/YogaStudio.Identity/Startup.cs
@@ -0,0 +1,44 @@
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YogaStudio.Identity
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddInMemoryApiResources(new List<ApiResource>())
+                .AddInMemoryIdentityResources(new List<IdentityResource>())
+                .AddInMemoryApiScopes(new List<ApiScope>())
+                .AddInMemoryClients(new List<Client>())
+                .AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
